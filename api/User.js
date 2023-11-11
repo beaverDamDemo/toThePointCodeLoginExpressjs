e260e23f2674bfbfe7dd57fb2cb8879e1ac0572b
beaverDamDemo <zdenevi@gmail.com>
2023-11-11 23:24:22 +0100

    login and registration is working

--- api/User.js
+++ api/User.js
@@ -1,91 +1,145 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const User =  require('./../models/User')
-const bcrypt = require('bcrypt')
+const User = require("./../models/User");
+const bcrypt = require("bcrypt");
 
-router.post('/signup', (req, res)=>{
-
-	console.log("req.body: ", req.body)
-
-
-	let {name, email, password, dateOfBirth} = req.body;
-	console.log("req.body: ", name)
-	console.log("req.body: ", email)
-	console.log("req.body: ", password)
-	console.log("req.body: ", dateOfBirth)
-
+router.post("/signup", (req, res) => {
+	let { name, email, password, dateOfBirth } = req.body;
 	name = name.trim();
 	email = email.trim();
 	password = password.trim();
 	dateOfBirth = dateOfBirth.trim();
-	if(name == "" || email == "" || password == "" || dateOfBirth == "") {
+	if (name == "" || email == "" || password == "" || dateOfBirth == "") {
 		res.json({
 			status: "FAILED",
-			message: "Empty input fields!"
-		})
-	} else if(!/^[a-zA-z]*$/.test(name)) {
+			message: "Empty input fields!",
+		});
+	} else if (!/^[a-zA-z0-9]*$/.test(name)) {
 		res.json({
 			status: "FAILED",
-			message: "Invalid name entered"
-		})
-	}else if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+			message: "Invalid name entered",
+		});
+	} else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
 		res.json({
 			status: "FAILED",
-			message: "Invalid email entered"
-		})
-	} else if(!new Date(dateOfBirth).getTime()) {
+			message: "Invalid email entered",
+		});
+	} else if (!new Date(dateOfBirth).getTime()) {
 		res.json({
 			status: "FAILED",
-			message: "Invalid date of birth entered"
-		})
-	} else if(password.length < 2){
+			message: "Invalid date of birth entered",
+		});
+	} else if (password.length < 2) {
 		res.json({
 			status: "FAILED",
-			message: "Password too short"
-		})
+			message: "Password too short",
+		});
 	} else {
-		User.find({email}).then(result=>{
-			if(result.length) {
+		User.find({ email })
+		.then((result) => {
+			if (result.length) {
+				console.log("FAILED Looks like email already exists");
 				res.json({
 					status: "FAILED",
-					message: "Looks like email already exists"
-				})
+					message: "Looks like email already exists",
+				});
 			} else {
-				// creating new user
+				console.log("creating new user");
+          // creating new user
 				const saltRounds = 10;
-				bcrypt.hash(password, saltRounds).then(hashedPassword=>{
+				bcrypt
+				.hash(password, saltRounds)
+				.then((hashedPassword) => {
 					const newUser = new User({
-						name, email, password: hashedPassword, dateOfBirth
-					})
-					newUser.save().then(result=>{
+						name,
+						email,
+						password: hashedPassword,
+						dateOfBirth,
+					});
+					newUser
+					.save()
+					.then((result) => {
 						res.json({
 							status: "SUCCESS",
-							message: "Signup OK", data: result})
-					}).catch(e=>{
+							message: "Signup OK",
+							data: result,
+						});
+					})
+					.catch((e) => {
 						res.json({
 							status: "FAILED",
-							message: "An error occurred while saving new user!"
-						})	
-					})
+							message: "An error occurred while saving new user!",
+						});
+					});
 				})
-				.catch(e=>{
+				.catch((e) => {
 					res.json({
 						status: "FAILED",
-						message: "An error occurred while hashing password!"
-					})	
-				})
+						message: "An error occurred while hashing password!",
+					});
+				});
 			}
-		}).catch(err=>{
+		})
+		.catch((err) => {
 			res.json({
 				status: "FAILED",
-				message: "Error while checking if user already exists"
-			})	
-		})
+				message: "Error while checking if user already exists",
+			});
+		});
 	}
-})
+});
 
-router.post('/signin', (req, res)=>{
+router.post("/signin", (req, res) => {
+	let { email, password } = req.body;
+	email = email.trim();
+	password = password.trim();
 
-})
+	if (email == "" || password == "") {
+		res.json({
+			status: "FAILED",
+			message: "Empty credentials supplied",
+		});
+	} else {
+		User.find({ email })
+		.then((data) => {
+			if (data) {
+				const hashedPassword = data[0].password;
+				bcrypt
+				.compare(password, hashedPassword)
+				.then((result) => {
+					if (result) {
+						res.json({
+							status: "SUCCESS",
+							message: "Signin successful",
+							data: data,
+						});
+					} else {
+						res.json({
+							status: "FAILED",
+							message: "Invalid password entered!",
+						});
+					}
+				})
+				.catch((err) => {
+					res.json({
+						status: "FAILED",
+						message: "An error occurred while comparing passwords",
+					});
+				});
+			} else {
+				res.json({
+					status: "FAILED",
+					message: "Invalid credentials entered!",
+				});
+			}
+		})
+		.catch((err) => {
+			res.json({
+				status: "FAILED",
+				message: "An error occurred while checking for existing user",
+			});
+		});
+	}
+});
 
 module.exports = router;
