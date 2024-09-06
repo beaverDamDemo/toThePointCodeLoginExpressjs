737499ff8858877f57d204f63803c0c02377a2ee
beaverDamDemo <zdenevi@gmail.com>
2024-09-06 15:06:43 +0200

    added sample api point

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const CarRouter = require('./api/Car');
 const bodyParser = require('express').json;
 const cors = require('cors')
 const corsOptions = {
-	origin: "*",
+	origin: 'http://localhost:3000',
 	optionsSuccessStatus: 200
 }
+app.use(cors());
 // app.use(bodyParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/User', cors(corsOptions), UserRouter)
 app.use('/Car', cors(corsOptions), CarRouter)
+
+app.get('/sample', (req, res) => {
+	res.json({ message: 'Sample: This is CORS-enabled for all origins!' });
+});
 
 app.listen(port, ()=>{
 	console.log(`Server is running on port ${port}`);
